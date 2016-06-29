408727b9e7ae4fc8ac9edf51eb134708115df9d1
Matt Swensen <swensen.matthew@gmail.com>
2016-06-28 22:55:34 -0600

    Adding URL removal checks to build and build status icon to read.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Source code for [mjswensen.com](http://mjswensen.com/).
+Source code for [mjswensen.com](http://mjswensen.com/). [![Build Status](https://travis-ci.org/mjswensen/mjswensen.github.io.svg?branch=master)](https://travis-ci.org/mjswensen/mjswensen.github.io)
--- cibuild.bash
+++ cibuild.bash
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+echo "Building site..."
 bundle exec jekyll build
+
+echo "Checking link and image URLs..."
 bundle exec htmlproofer ./_site --url-ignore /linkedin.com/
+
+echo "Making sure CSS files didn't make it into the sitemap..."
 [ $(grep \\.css _site/sitemap.xml -c) -eq 0 ]
+
+PROD_SITEMAP="./prod-sitemap.xml"
+
+echo "Downloading production sitemap..."
+curl mjswensen.com/sitemap.xml > $PROD_SITEMAP
+
+echo "Ensuring that all previously existing picks are still there..."
+grep -o '\/picks\/[^\<]*' $PROD_SITEMAP | xargs -t -I % grep -q % _site/sitemap.xml
+
+echo "Ensuring that all previously existing presentations are still there..."
+grep -o '\/presentations\/[^\<]*' $PROD_SITEMAP | xargs -t -I % grep -q % _site/sitemap.xml
+
+echo "Ensuring that all previously existing posts are still there..."
+grep -o '\/blog\/[^\<]*' $PROD_SITEMAP | xargs -t -I % grep -q % _site/sitemap.xml
+
+echo "Cleanup..."
+rm $PROD_SITEMAP
